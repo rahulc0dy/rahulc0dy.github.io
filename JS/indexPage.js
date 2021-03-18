1beabb2d25bd3abc06f1e10e8b4217a9373e8146
rahulc0dy <rahulchakraborty337@gmail.com>
2021-03-18 14:26:47 +0530

    screen alert

--- JS/indexPage.js
+++ JS/indexPage.js
@@ -16,3 +16,13 @@ function topFunction() {
   document.body.scrollTop = 0; // For Safari
   document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
 }
+
+
+function myFunction(x) {
+  if (x.matches) { // If media query matches
+    alert("This page is optimised for desktop. Some contents may not display properly on phone");
+  }}
+
+var x = window.matchMedia("(max-width: 700px)")
+myFunction(x) // Call listener function at run time
+x.addListener(myFunction) // Attach listener function on state changes
